c07da24ae238c2bdb9c112ceaabb42118bb3f3bf
hari <harivicky7777@gmail.com>
2024-02-14 13:31:43 +0530

    ui Changes

--- videoupload/models.py
+++ videoupload/models.py
@@ -1,9 +1,12 @@
 from django.db import models
 from django.contrib.auth.models import User
 
+def video_file_path(instance, filename):
+    return filename
+
 class Video(models.Model):
     title = models.CharField(max_length=255)
     description = models.TextField()
-    video_file = models.FileField(upload_to='videos/')
+    video_file = models.FileField(upload_to=video_file_path)
     uploaded_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
